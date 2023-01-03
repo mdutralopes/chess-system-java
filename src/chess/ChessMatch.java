d7fe398f60cf5e23bed4eb88f14b7b781bf9bd3a
Marcos Lopes <Z348711@corp.clarobr>
2023-01-03 08:30:21 -0300

    Colocando as peças no tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,6 +8,7 @@ public class ChessMatch {
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -16,5 +17,9 @@ public class ChessMatch {
 			for (int j = 0; j < board.getColumns(); j++)
 				mat[i][j] = (ChessPiece) board.piece(i, j);
 	    return mat;
+	}
+	
+	private void initialSetup() {
+		
 	}
 }
