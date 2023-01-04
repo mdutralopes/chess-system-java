accbc2452965b3e0f63900dc211968d4b76ded96
Marcos Lopes <Z348711@corp.clarobr>
2023-01-04 10:32:36 -0300

    Imprimindo os movimentos possíveis

--- src/application/UI.java
+++ src/application/UI.java
@@ -42,24 +42,26 @@ public class UI {
 		}
 		catch (RuntimeException e) {
 			throw new InputMismatchException("Erro lendo posição da peça! Valores válidos entre a1 e h8!");
-		}
-		
+		}	
 	}
 	
-	public static void printBoard(ChessPiece[][] pieces) {	
+	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
-				printPiece(pieces[i][j]);
-			}	
+				printPiece(pieces[i][j], false);
+			}
 			System.out.println();
 		}
 		System.out.println("  a b c d e f g h");
 	}
 	
-	private static void printPiece(ChessPiece piece) {
-		if (piece == null) {
-            System.out.print("-");
+	private static void printPiece(ChessPiece piece, boolean background) {
+		if (background) {
+			System.out.print(ANSI_BLUE_BACKGROUND);
+		}
+    	if (piece == null) {
+            System.out.print("-" + ANSI_RESET);
         }
         else {
             if (piece.getColor() == Color.WHITE) {
@@ -70,5 +72,15 @@ public class UI {
             }
         }
         System.out.print(" ");
-	}	
+	}
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j], possibleMoves[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("  a b c d e f g h");
+	}
 }
